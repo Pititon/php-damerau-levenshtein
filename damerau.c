2afe6498cb424227d8a4b85110ff906b545dd79a
Alexey Korchevsky <alexey.korchevsky@wikimart.ru>
2012-02-21 11:47:22 +0400

    fix using mbstring ini

--- damerau.c
+++ damerau.c
@@ -4,19 +4,14 @@
 
 #endif
 
-
-
-
-
 #include "php.h"
 #include "php/Zend/zend_API.h"
 #include "php_ini.h"
 #include "php_damerau.h"
-#include "php/ext/mbstring/mbstring.h"
-#include "php/ext/mbstring/libmbfl/mbfl/mbfl_string.h"
-#include "php/ext/mbstring/libmbfl/mbfl/mbfl_language.h"
-#include "php/ext/mbstring/libmbfl/mbfl/mbfilter.h"
-#include "php/main/php.h"
+#include "ext/mbstring/mbstring.h"
+#include "ext/mbstring/libmbfl/mbfl/mbfl_string.h"
+#include "ext/mbstring/libmbfl/mbfl/mbfl_language.h"
+#include "ext/mbstring/libmbfl/mbfl/mbfilter.h"
 
 
 ZEND_BEGIN_ARG_INFO(arginfo_damerau_levenshtein, 0)
@@ -28,8 +23,6 @@ ZEND_BEGIN_ARG_INFO(arginfo_damerau_levenshtein, 0)
     ZEND_ARG_INFO(0, cost_tran)
 ZEND_END_ARG_INFO()
 
-ZEND_EXTERN_MODULE_GLOBALS(mbstring)
-
 static function_entry damerau_levenshtein_functions[] = {
 
   PHP_FE(damerau_levenshtein, arginfo_damerau_levenshtein)
@@ -38,21 +31,30 @@ static function_entry damerau_levenshtein_functions[] = {
 
 };
 
+
+
+/* {{{ damerau ini */
+
+ZEND_DECLARE_MODULE_GLOBALS(damerau)
+
+static PHP_GINIT_FUNCTION(damerau);
+static PHP_RINIT_FUNCTION(damerau);
+
 zend_module_entry damerau_module_entry = {
-#if ZEND_MODULE_API_NO >= 20010901
-  STANDARD_MODULE_HEADER,
-#endif
-  PHP_DAMERAU_EXTNAME,
-  damerau_levenshtein_functions,
-  NULL,
-  NULL,
-  NULL,
-  NULL,
-  NULL,
-#if ZEND_MODULE_API_NO >= 20010901
-  PHP_DAMERAU_VERSION,
-#endif
-  STANDARD_MODULE_PROPERTIES
+    STANDARD_MODULE_HEADER,
+	PHP_DAMERAU_EXTNAME,
+	damerau_levenshtein_functions,
+	NULL, // PHP_MINIT(damerau),
+	NULL, // PHP_MSHUTDOWN(damerau),
+	PHP_RINIT(damerau),
+	NULL, // PHP_RSHUTDOWN(damerau),
+	NULL, // PHP_MINFO(damerau),
+    NO_VERSION_YET,
+    PHP_MODULE_GLOBALS(damerau),
+    PHP_GINIT(damerau),
+    NULL, // PHP_GSHUTDOWN(damerau),
+    NULL,
+	STANDARD_MODULE_PROPERTIES_EX
 };
 
 #ifdef COMPILE_DL_DAMERAU
@@ -61,89 +63,124 @@ ZEND_GET_MODULE(damerau)
 
 #endif
 
-#define LEVENSHTEIN_MAX_LENGTH 255
+#define MBSTRG_EX(v) (damerau_globals.v)
 
-/* {{{ reference_damerau_levenshtein
- * reference implementation, only optimized for memory usage and speed */
-static int reference_damerau_levenshtein(const char *first, int firstLength, const char *second, int secondLength, int cost_ins, int cost_sub, int cost_del, int cost_tran )
+int _php_mb_ini_mbstring_internal_encoding_set(const char *new_value, uint new_value_length TSRMLS_DC)
 {
-    int *currentRow, *previousRow, *transpositionRow, *tempRow;
-    int i, j, t, from, to, cost, value;
-    char lastSecondCh = 0, secondCh, firstCh, lastFirstCh;
-    
-    if (firstLength == 0) {
-        return secondLength * cost_ins;
-    }
-    if (secondLength == 0) {
-        return firstLength * cost_del;
-    }
-    
-    if ((firstLength > LEVENSHTEIN_MAX_LENGTH) || (secondLength > LEVENSHTEIN_MAX_LENGTH)) {
-        return -1;
-    }
-    
-    if(firstLength > secondLength){
-        char *tmp = first;
-        first = second;
-        second = tmp;
-        int tmpl = secondLength;
-        secondLength = firstLength;
-        firstLength = tmpl;
-    }
+	enum mbfl_no_encoding no_encoding;
+  	const char *enc_name = NULL;
+  	uint enc_name_len = 0;
+   
+  	no_encoding = new_value ? mbfl_name2no_encoding(new_value):
+  				mbfl_no_encoding_invalid;
+	if (no_encoding != mbfl_no_encoding_invalid) {
+  		enc_name = new_value;
+  		enc_name_len = new_value_length;
+  	} else {
+  		switch (MBSTRG_EX(language)) {
+  			case mbfl_no_language_uni:
+  				enc_name = "UTF-8";
+  				enc_name_len = sizeof("UTF-8") - 1;
+  				break;
+  			case mbfl_no_language_japanese:
+  				enc_name = "EUC-JP";
+  				enc_name_len = sizeof("EUC-JP") - 1;
+  				break;
+  			case mbfl_no_language_korean:
+  				enc_name = "EUC-KR";
+  				enc_name_len = sizeof("EUC-KR") - 1;
+  				break;
+  			case mbfl_no_language_simplified_chinese:
+  				enc_name = "EUC-CN";
+  				enc_name_len = sizeof("EUC-CN") - 1;
+  				break;
+  			case mbfl_no_language_traditional_chinese:
+  				enc_name = "EUC-TW";
+  				enc_name_len = sizeof("EUC-TW") - 1;
+  				break;
+  			case mbfl_no_language_russian:
+  				enc_name = "KOI8-R";
+  				enc_name_len = sizeof("KOI8-R") - 1;
+  				break;
+  			case mbfl_no_language_german:
+  				enc_name = "ISO-8859-15";
+  				enc_name_len = sizeof("ISO-8859-15") - 1;
+  				break;
+  			case mbfl_no_language_armenian:
+  				enc_name = "ArmSCII-8";
+  				enc_name_len = sizeof("ArmSCII-8") - 1;
+  				break;
+  			case mbfl_no_language_turkish:
+  				enc_name = "ISO-8859-9";
+  				enc_name_len = sizeof("ISO-8859-9") - 1;
+  				break;
+  			default:
+  				enc_name = "ISO-8859-1";
+  				enc_name_len = sizeof("ISO-8859-1") - 1;
+  				break;
+  		}
+  		no_encoding = mbfl_name2no_encoding(enc_name);
+  	}
+	MBSTRG_EX(internal_encoding) = no_encoding;
+	MBSTRG_EX(current_internal_encoding) = no_encoding;
+#if HAVE_MBREGEX
+	{
+		const char *enc_name = new_value;
+		if (FAILURE == php_mb_regex_set_default_mbctype(enc_name TSRMLS_CC)) {
+			/* falls back to EUC-JP if an unknown encoding name is given */
+			enc_name = "EUC-JP";
+			php_mb_regex_set_default_mbctype(enc_name TSRMLS_CC);
+		}
+		php_mb_regex_set_mbctype(new_value TSRMLS_CC);
+	}
+#endif
+	return SUCCESS;
+}
 
-    currentRow = safe_emalloc((firstLength + 1), sizeof(int), 0);
-    previousRow = safe_emalloc((firstLength + 1), sizeof(int), 0);
-    transpositionRow = safe_emalloc((firstLength + 1), sizeof(int), 0);
+static PHP_INI_MH(OnUpdate_damerau_language)
+{
+    enum mbfl_no_language no_language;
+
+	no_language = mbfl_name2no_language(new_value);
+	if (no_language == mbfl_no_language_invalid) {
+		MBSTRG_EX(language) = mbfl_no_language_neutral;
+		return FAILURE;
+	}
+	MBSTRG_EX(language) = no_language;
     
-    for (i=0; i<=firstLength; i++){
-        previousRow[i] = i * cost_ins;
-        currentRow[i] = 0;
-        transpositionRow[i] = 0;
-    }
-    
-    for(i = 1; i <= secondLength; i++){
-        secondCh = second[i-1];
-        currentRow[0] = i;
-        
-        from = i-secondLength - 1;
-        if(from < 1) from = 1;
-        to = i+secondLength + 1;
-        if(to > firstLength) to = firstLength;
-        
-        lastFirstCh = 0;
-        for(j=from; j <= to; j++){
-            firstCh = first[j-1];
-            
-            cost = (firstCh == secondCh)? 0 : 1;
-            value = currentRow[j-1] + cost_ins; // insertion
-            t = previousRow[j] + cost_del; // deletion
-            if(value > t) value = t;
-            t = previousRow[j-1] + (cost?cost_sub:0); // substitution
-            if(value > t) value = t;
-            
-            if(firstCh == lastSecondCh && secondCh == lastFirstCh){
-                t = transpositionRow[j-2] + (cost?cost_tran:0); // transposition
-                if(value > t) value = t; 
-            }
-            
-            currentRow[j] = value;
-            lastFirstCh = firstCh;
-        }
-        lastSecondCh = secondCh;
-        
-        tempRow = transpositionRow;
-        transpositionRow = previousRow;
-        previousRow = currentRow;
-        currentRow = tempRow;
-    }
-    
-    cost = previousRow[firstLength];
-    efree(currentRow);
-    efree(previousRow);
-    efree(transpositionRow);
-    return cost;
+    return SUCCESS;
+}
+
+static PHP_INI_MH(OnUpdate_damerau_internal_encoding)
+{
+    if (stage == PHP_INI_STAGE_STARTUP || stage == PHP_INI_STAGE_SHUTDOWN
+			|| stage == PHP_INI_STAGE_RUNTIME) {
+        return _php_mb_ini_mbstring_internal_encoding_set(new_value, new_value_length TSRMLS_CC);
+	} else {
+		return SUCCESS;
+	}
+}
+
+PHP_INI_BEGIN()
+	PHP_INI_ENTRY("mbstring.language", "neutral", PHP_INI_ALL, OnUpdate_damerau_language)
+	PHP_INI_ENTRY("mbstring.internal_encoding", NULL, PHP_INI_ALL, OnUpdate_damerau_internal_encoding)
+PHP_INI_END()
+
+static PHP_GINIT_FUNCTION(damerau)
+{
+	damerau_globals->language = mbfl_no_language_uni;
+	damerau_globals->internal_encoding = mbfl_no_encoding_utf8;
+	damerau_globals->current_internal_encoding = damerau_globals->internal_encoding;
+}
+
+static PHP_RINIT_FUNCTION(damerau)
+{
+    MBSTRG_EX(current_internal_encoding) = MBSTRG_EX(internal_encoding);
+	return SUCCESS;
 }
 /* }}} */
+
+#define LEVENSHTEIN_MAX_LENGTH 255
 
 /* {{{ reference_damerau_levenshtein
  * reference implementation, only optimized for memory usage and speed */
@@ -160,17 +197,17 @@ static int reference_mb_damerau_levenshtein(
     
     int firstLength, secondLength;
     mbfl_string_init(&first);
-    first.no_language = MBSTRG(language);
-	first.no_encoding = MBSTRG(current_internal_encoding);
+    first.no_language = MBSTRG_EX(language);
+	first.no_encoding = MBSTRG_EX(current_internal_encoding);
     first.val = (unsigned char *)f;
     first.len = fl;
 
     mbfl_string_init(&second);
-    second.no_language = MBSTRG(language);
-    second.no_encoding = MBSTRG(current_internal_encoding);
+    second.no_language = MBSTRG_EX(language);
+    second.no_encoding = MBSTRG_EX(current_internal_encoding);
     second.val = (unsigned char *)s;
     second.len = sl;
-    
+
 	firstLength = mbfl_strlen(&first);
     secondLength = mbfl_strlen(&second);
     
@@ -209,10 +246,10 @@ static int reference_mb_damerau_levenshtein(
     
     mbfl_string_init(&firstCh);
     mbfl_string_init(&secondCh);
-    firstCh.no_language = MBSTRG(language);
-    firstCh.no_encoding = MBSTRG(current_internal_encoding);
-    secondCh.no_language = MBSTRG(language);
-    secondCh.no_encoding = MBSTRG(current_internal_encoding);
+    firstCh.no_language = MBSTRG_EX(language);
+    firstCh.no_encoding = MBSTRG_EX(current_internal_encoding);
+    secondCh.no_language = MBSTRG_EX(language);
+    secondCh.no_encoding = MBSTRG_EX(current_internal_encoding);
     
     for(i = 1; i <= secondLength; i++){
         retSecondCh = mbfl_substr(&second, &secondCh, i-1, 1);
--- php_damerau.h
+++ php_damerau.h
@@ -3,8 +3,21 @@
 #define PHP_DAMERAU_VERSION "1.0"
 #define PHP_DAMERAU_EXTNAME "damerau"
 
+#include "ext/mbstring/libmbfl/mbfl/mbfl_string.h"
+#include "ext/mbstring/libmbfl/mbfl/mbfl_language.h"
+#include "ext/mbstring/libmbfl/mbfl/mbfilter.h"
+
 PHP_FUNCTION(damerau_levenshtein);
 
 extern zend_module_entry damerau_module_entry;
 #define phpext_damerau_ptr &damerau_module_entry
+
+ZEND_BEGIN_MODULE_GLOBALS(damerau)
+	enum mbfl_no_language language;
+	enum mbfl_no_encoding internal_encoding;
+	enum mbfl_no_encoding current_internal_encoding;
+ZEND_END_MODULE_GLOBALS(damerau)
+
+
+        
 #endif
